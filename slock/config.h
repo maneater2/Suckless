00661c744c2253c02ad22511cb11d5b3696ac434
maneater2 <pielover@tuta.io>
2024-10-26 21:07:22 +0300

    first commit

---
+++ slock/config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "light";
+static const char *group = "wheel";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#ebcb8b",   /* during input */
+	[FAILED] = "#bf616a",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
